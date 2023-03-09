80bc9255e84336838e6cef4be0c5b0a079895446
Jeremy Polanco Lara <jeremyinf26@gmail.com>
2023-03-08 23:32:49 -0400

    add models, and add readonly columns in the admin file

--- GestionInventario/GestionInventarioApp/admin.py
+++ GestionInventario/GestionInventarioApp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models import Categoria, Producto
 
 # Register your models here.
+
+class Categoria_admin (admin.ModelAdmin): 
+    readonly_fields = ('created','updated')
+
+class Producto_admin (admin.ModelAdmin): 
+    readonly_fields = ('created','updated')
+    list_display = ('name','price')
+
+admin.site.register(Categoria, Categoria_admin)
+admin.site.register(Producto, Producto_admin)
--- GestionInventario/GestionInventarioApp/models.py
+++ GestionInventario/GestionInventarioApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria (models.Model): 
+    name = models.CharField(max_length=30)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
+    def __str__(self):
+     return self.name
+    
+class Producto (models.Model): 
+    name = models.CharField(max_length=30)
+    codigos_costo = (
+        ('1', 'Muy Barato'),
+        ('2', 'Barato'),
+        ('3', 'Intermedio'),
+        ('4', 'Alto'),
+        ('5', 'Muy Alto'),
+    )
+    categoria = models.ManyToManyField(Categoria)
+    price = models.FloatField()
+    cantidad = models.IntegerField()
+    codigo_costo = models.CharField(choices= codigos_costo)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
+
+
+
